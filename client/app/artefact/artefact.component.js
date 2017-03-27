5f6f5033a29babe2641c07e4ae9871f7b10dda9f
Mark Turner <mark.turner@ncl.ac.uk>
2017-03-27 11:45:14 +0100

    Styled artefact view

--- client/app/artefact/artefact.component.js
+++ client/app/artefact/artefact.component.js
@@ -4,8 +4,7 @@ import routing from './artefact.routes';
 
 export class ArtefactController {
 
-    awesomeThings = [];
-    newThing = '';
+    artefact = {};
 
     /*@ngInject*/
     constructor($http, $state) {
@@ -14,17 +13,50 @@ export class ArtefactController {
     }
 
     $onInit() {
-        var artefactId = this.$state.params.artefactId;
+        let artefactId = this.$state.params.artefactId;
+        let artefact = {};
 
         if(artefactId) {
             this.$http.get('/api/artefacts/' + artefactId)
                 .then(response => {
-                    this.artefact = response.data;
+                    artefact = response.data;
+
+                    artefact.periods.forEach(function(period, index){
+                        let start = period.periodStart < 0 ? (period.periodStart * -1) + 'BC' : period.periodStart + 'AD';
+                        let end = period.periodEnd < 0 ? (period.periodEnd * -1) + 'BC' : period.periodEnd + 'AD';
+                        artefact.periods[index].display = start + ' to ' + end;
+                    });
+
+                    artefact.images = _.filter(response.data.references, function (reference) {
+                        return reference.type === 'IMAGE' && reference.contents.indexOf('1024') !== -1;
+                    });
+
+                    artefact.references = _.filter(response.data.references, function (reference) {
+                        return reference.type === 'TEXT';
+                    });
+
+                    this.artefact = artefact;
                 });
         } else {
             this.$http.get('/api/artefacts/random')
                 .then(response => {
-                    this.artefact = response.data;
+                    artefact = response.data[0];
+
+                    artefact.periods.forEach(function(period, index){
+                        let start = period.periodStart < 0 ? (period.periodStart * -1) + 'BC' : period.periodStart + 'AD';
+                        let end = period.periodEnd < 0 ? (period.periodEnd * -1) + 'BC' : period.periodEnd + 'AD';
+                        artefact.periods[index].display = start + ' to ' + end;
+                    });
+
+                    artefact.images = _.filter(response.data[0].references, function (reference) {
+                        return reference.type === 'IMAGE' && reference.contents.indexOf('1024') !== -1;
+                    });
+
+                    artefact.references = _.filter(response.data[0].references, function (reference) {
+                        return reference.type === 'TEXT';
+                    });
+
+                    this.artefact = artefact;
                 });
         }
     }
