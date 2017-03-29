df1effa759ea22e4c914364edbea98fe1448f8e9
Mark Turner <mark.turner@ncl.ac.uk>
2017-03-29 11:18:20 +0100

    Added case study modal

--- client/app/artefact/artefact.component.js
+++ client/app/artefact/artefact.component.js
@@ -2,24 +2,59 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import routing from './artefact.routes';
 
+function ModalInstanceCtrl($uibModalInstance, artefact) {
+
+    this.artefact = artefact;
+    this.ok = function () {
+        $uibModalInstance.close();
+    };
+
+    let $ctrl = this;
+}
+
+ModalInstanceCtrl.$inject = ['$uibModalInstance', 'artefact'];
+
 export class ArtefactController {
 
     artefact = {};
 
     /*@ngInject*/
-    constructor($http, $state) {
+    constructor($http, $state, $uibModal) {
         this.$http = $http;
         this.$state = $state;
+        this.$uibModal = $uibModal;
     }
 
     $onInit() {
+
+        let $uibModal = this.$uibModal;
         let artefactId = this.$state.params.artefactId;
         let artefact = {};
+
+        function camelize(str) {
+            let camelcase = str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
+                if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+                return index == 0 ? match.toLowerCase() : match.toUpperCase();
+            });
+
+            camelcase = camelcase.replace('/','');
+            camelcase = camelcase.replace('-','');
+
+            return camelcase;
+        }
 
         if(artefactId) {
             this.$http.get('/api/artefacts/' + artefactId)
                 .then(response => {
                     artefact = response.data;
+
+                    if(artefact.caseStudy){
+                        let caseStudy = {};
+                        artefact.caseStudy.forEach(function(property){
+                            caseStudy[camelize(property.name)] = property.value;
+                        });
+                        artefact.caseStudy = caseStudy;
+                    }
 
                     artefact.periods.forEach(function(period, index){
                         let start = period.periodStart < 0 ? (period.periodStart * -1) + 'BC' : period.periodStart + 'AD';
@@ -59,6 +94,24 @@ export class ArtefactController {
                     this.artefact = artefact;
                 });
         }
+
+        this.openModal = function(artefact, modalClass = 'modal-default') {
+
+            console.log(artefact);
+
+            return $uibModal.open({
+                template: require('./case-study.modal.html'),
+                windowClass: modalClass,
+                size: 'lg',
+                controller: ModalInstanceCtrl,
+                controllerAs: '$ctrl',
+                resolve: {
+                    artefact: function () {
+                        return artefact;
+                    }
+                }
+            });
+        }
     }
 }
 
@@ -68,4 +121,5 @@ export default angular.module('cuttingEdgeDockerApp.artefact', [uiRouter])
         template: require('./artefact.view.html'),
         controller: ArtefactController
     })
+    .controller('ModalInstanceCtrl', ModalInstanceCtrl)
     .name;
